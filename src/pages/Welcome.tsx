c61363d5e9343b238b1c5e113a6508aa970ec7ec
Daniel Ferreira <danielfferreirabr@gmail.com>
2021-04-19 16:14:23 -0300

    Styling the texts and centralizing the container

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,18 +1,42 @@
 import React from 'react';
-import { View, Text, Image } from 'react-native';
+import { SafeAreaView, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
 import wateringImg from '../assets/watering.png';
 
 
 export function Welcome() {
   return (
-    <View>
-      <Text>
+    <SafeAreaView style={styles.container}>
+
+      <Text style={styles.title}>
         Gerencie
         suas plantas de
         forma fácil
      </Text>
 
       <Image source={wateringImg} />
-    </View>
+
+      <Text style={styles.subtitle}>
+        Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você
+        sempre que precisar.
+      </Text>
+
+      <TouchableOpacity style={styles.btn}>
+        <Text>
+          >
+        </Text>
+      </TouchableOpacity>
+    </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 15,
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  },
+  title: {
+    fontSize: 32,
+  }
+});
