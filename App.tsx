adb99e8b13c77a80d25a2325edad84695a87818d
Daniel Ferreira <danielfferreirabr@gmail.com>
2021-04-19 13:45:47 -0300

    Importing the component from the src directory

--- App.tsx
+++ App.tsx
@@ -1,23 +1,9 @@
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Welcome } from './src/pages/Welcome';
 
 export default function App() {
   return (
-    <View style={style.container}>
-      <Text>
-        Oiiii
-      </Text>
-      <Text>
-        Nlw#5
-      </Text>
-    </View>
+    <Welcome />
   )
 }
 
-const style = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
-})
