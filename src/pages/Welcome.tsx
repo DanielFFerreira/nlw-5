d59c3b618519a294a6bb85fea9e7bfd791d98955
Daniel Ferreira <danielfferreirabr@gmail.com>
2021-04-19 13:39:56 -0300

    first commit and starting structure with expo init

---
+++ src/pages/Welcome.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text } from 'react-native'
+
+
+export function Welcome() {
+  return (
+    <View>
+      <Text>
+        Gerencie
+        suas plantas de
+        forma fácil
+     </Text>
+    </View>
+  )
+}
