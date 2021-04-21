b6e6558a05d6245ee1b5f8d51e63ad97b08dfc60
Daniel Ferreira <danielfferreirabr@gmail.com>
2021-04-21 13:08:16 -0300

    Import stylecheet e area view exporting component and creating a new style

---
+++ src/pages/UserIdentification.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet
+} from 'react-native';
+
+export function UserIdentification() {
+  return (
+    <SafeAreaView style={styles.container}>
+
+    </SafeAreaView>
+  )
+}
+const styles = StyleSheet.create({
+  container: {
+
+  }
+});
