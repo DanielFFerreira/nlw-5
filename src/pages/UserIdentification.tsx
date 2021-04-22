a525bb48fdb331009352d298e7ae939255aa9873
Daniel Ferreira <danielfferreirabr@gmail.com>
2021-04-22 11:56:03 -0300

    Importing button component and creating state of input behavior

--- src/pages/UserIdentification.tsx
+++ src/pages/UserIdentification.tsx
@@ -1,18 +1,126 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   SafeAreaView,
-  StyleSheet
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  KeyboardAvoidingView,
+  Platform
 } from 'react-native';
 
+// Import components
+import { Button } from '../components/Button';
+
+// Import styles
+import colors from '../styles/colors';
+import fonts from '../styles/fonts';
+
 export function UserIdentification() {
+
+  const [isFocused, setisFocused] = useState(false);
+  const [Filled, setFilled] = useState(false);
+  const [name, setname] = useState<string>();
+
+  function handleInputBlur() {
+    setisFocused(false);
+    setFilled(!!name);
+  }
+
+  function handleInputChange(value: string) {
+    setFilled(!!value);
+    setname(value);
+  }
+
+  function handleInputFocus() {
+    setisFocused(true);
+  }
+
   return (
     <SafeAreaView style={styles.container}>
+      <KeyboardAvoidingView
+        style={styles.container}
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      >
+        <View style={styles.content}>
+          <View style={styles.form}>
+            <View style={styles.header}>
+              <Text style={styles.emoji}>
+                {Filled ? '😀' : '🙂'}
+              </Text>
+              <Text style={styles.title}>
+                Como podemos {'\n'}
+                chamar você?
+              </Text>
+            </View>
+            <TextInput
+              style={[
+                styles.input,
+                (isFocused || setFilled) && {
+                  borderColor: colors.green,
+                }
+              ]}
+              placeholder='Digite o seu nome'
+              onBlur={handleInputBlur}
+              onFocus={handleInputFocus}
+              onChangeText={handleInputChange}
+            />
 
+            <View style={styles.footer}>
+              <Button />
+            </View>
+          </View>
+
+        </View>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   )
 }
 const styles = StyleSheet.create({
   container: {
-
+    flex: 1,
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  content: {
+    flex: 1,
+    width: '100%'
+  },
+  form: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingHorizontal: 54,
+    alignItems: 'center',
+  },
+  header: {
+    alignItems: 'center'
+  },
+  emoji: {
+    fontSize: 44,
+  },
+  input: {
+    borderBottomWidth: 1,
+    borderColor: colors.gray,
+    color: colors.heading,
+    width: '100%',
+    fontSize: 18,
+    marginTop: 50,
+    padding: 10,
+    textAlign: 'center'
+  },
+  title: {
+    fontSize: 24,
+    lineHeight: 32,
+    textAlign: 'center',
+    color: colors.heading,
+    fontFamily: fonts.heading,
+    marginTop: 15
+  },
+  footer: {
+    width: '100%',
+    marginTop: 40,
+    paddingHorizontal: 20,
   }
+
 });
