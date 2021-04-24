bd1057e9ac5851e506d7abdd4335b1ee057199da
Daniel Ferreira <danielfferreirabr@gmail.com>
2021-04-23 22:37:43 -0300

    Importing and switching to routes react-navigation

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import AppLoading from 'expo-app-loading';
-import { Confirmation } from './src/pages/Confirmation';
+
+import Routes from './src/routes';
 
 import {
   useFonts,
   Jost_400Regular,
   Jost_600SemiBold
 } from '@expo-google-fonts/jost';
+import { StackRouter } from '@react-navigation/routers';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -19,7 +21,7 @@ export default function App() {
   }
 
   return (
-    <Confirmation />
+    <Routes />
   )
 }
 
