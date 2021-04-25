b24acb5f1a46905e5d65ae27f14e3cc5d522658d
Daniel Ferreira <danielfferreirabr@gmail.com>
2021-04-25 05:04:56 -0300

    Creating the route and importing the plantSelection component

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -4,6 +4,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { Welcome } from '../pages/Welcome';
 import { UserIdentification } from '../pages/UserIdentification';
 import { Confirmation } from '../pages/Confirmation';
+import { PlantSelect } from '../pages/PlantSelect';
 
 // Import styles
 import colors from '../styles/colors';
@@ -33,6 +34,11 @@ const AppRoutes: React.FC = () => (
     <stackRoutes.Screen
       name='Confirmation'
       component={Confirmation}
+    />
+
+    <stackRoutes.Screen
+      name='PlantSelect'
+      component={PlantSelect}
     />
 
 
