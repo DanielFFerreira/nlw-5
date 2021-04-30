f75313537e6f837d0d0ecdc15565ad27f5ff32f0
Daniel Ferreira <danielfferreirabr@gmail.com>
2021-04-30 02:09:58 -0300

    Importing components and fixing the positioning of the list of cards

--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -2,16 +2,45 @@ import React from 'react';
 import {
   View,
   Text,
-  StyleSheet
+  StyleSheet,
+  FlatList
 } from 'react-native';
+import { EnviromentButton } from '../components/EnviromentButton';
 
 import { Header } from '../components/Header';
 import colors from '../styles/colors';
+import fonts from '../styles/fonts';
 
 export function PlantSelect() {
   return (
     <View style={styles.container}>
-      <Header />
+      <View style={styles.header}>
+        <Header />
+
+        <Text style={styles.title}>
+          Em qual ambiente
+        </Text>
+
+        <Text style={styles.subtitle}>
+          você quer colocar sua planta ?
+        </Text>
+
+      </View>
+
+      <View>
+        <FlatList 
+          data={[1,2,3,4,5]}
+          renderItem={({item}) => (
+            <EnviromentButton 
+              title="Cozinha" 
+              active
+            />
+          )}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={styles.enviromentList}
+        />
+      </View>
     </View>
   )
 }
@@ -20,5 +49,28 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: colors.background,
+  },
+  header: {
+    paddingHorizontal: 30
+  },
+  title: {
+    fontSize: 17,
+    color: colors.heading,
+    fontFamily: fonts.heading,
+    lineHeight: 20,
+    marginTop: 15
+  },
+  subtitle: {
+    fontFamily: fonts.text,
+    fontSize: 17,
+    lineHeight: 20,
+    color: colors.heading,
+  },
+  enviromentList: {
+    height: 40,
+    justifyContent: 'center',
+    paddingBottom: 5,
+    marginLeft: 42,
+    marginVertical: 32
   }
 });
